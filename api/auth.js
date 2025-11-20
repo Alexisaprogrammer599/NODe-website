ab07e104e2f8a4b285406e5d3f8eb30b6f62f335
Alexisaprogrammer599 <alexisagamer599@gmail.com>
2025-11-20 18:04:10 +1100

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,10 +1,8 @@
 // api/auth.js
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
   const { code } = req.query;
   if (!code) {
-    res.status(400).json({ error: "Missing code" });
+    res.status(400).json({ error: "Missing ?code" });
     return;
   }
 
@@ -16,10 +14,11 @@ export default async function handler(req, res) {
   const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
     headers: { "Accept": "application/json" },
-    body: params,
+    body: params
   });
 
-  const tokenData = await tokenRes.json();
+  const tokenJson = await tokenRes.json();
+
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json(tokenData);
+  res.status(200).json(tokenJson);
 }
